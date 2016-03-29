041823a6f0c9afa262a7feecadae09808edafa01
Adam Pfister <apfister@esri.com>
2016-03-29 08:47:07 -0500

    env locationType update again.

--- config/environment.js
+++ config/environment.js
@@ -91,7 +91,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    // ENV.locationType = 'hash';
+    ENV.locationType = 'auto';
     ENV.baseURL = '/sdg-dash/';
     // ENV.baseURL = '/';
     ENV.sdgApi = 'https://sdg-api.herokuapp.com/';
